9493f0d0126d5b42ca18157f529107957ecf78f0
Mr.L <admin@duxphp.com>
2021-06-24 17:49:02 +0800

    update

--- src/System/Providers/AdminServiceProvider.php
+++ src/System/Providers/AdminServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\System\Providers;
 
+use Duxravel\Core\Util\Hook;
 use Duxravel\Core\Util\Menu;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
@@ -42,6 +43,13 @@ class AdminServiceProvider extends ServiceProvider
 
         // 注册数据库目录
         $this->loadMigrationsFrom(realpath(__DIR__ . '/../../../database/migrations'));
+
+        // 注册安装数据
+        if (\Request::is('install/*')) {
+            app(Hook::class)->add('service', 'type', 'getInstallData', function () {
+                return \Duxravel\System\Seeders\DatabaseSeeder::class;
+            });
+        }
 
     }
 }
