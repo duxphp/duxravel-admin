8dea4e98343486040fae9d777ecf1af361f4138b
Mr.L <admin@duxphp.com>
2021-07-09 13:50:39 +0800

    修正php8兼容问题

--- src/Tools/Admin/MenuExpend.php
+++ src/Tools/Admin/MenuExpend.php
@@ -8,44 +8,44 @@ class MenuExpend extends \Modules\System\Admin\Expend
 {
     public int $menuId;
 
-    public function index($menuId)
+    public function index($menuId = 0)
     {
         $this->menuId = $menuId;
         return parent::index();
     }
 
-    public function add($menuId)
+    public function add($menuId = 0)
     {
         $this->menuId = $menuId;
         return parent::add();
     }
 
-    public function edit($menuId, $id)
+    public function edit($menuId = 0, $id)
     {
         $this->menuId = $menuId;
         return parent::edit($id);
     }
 
-    public function page($menuId, $id = 0)
+    public function page($menuId = 0, $id = 0)
     {
         $this->menuId = $menuId;
         return parent::page($id);
     }
 
-    public function save($menuId, $id = 0)
+    public function save($menuId = 0, $id = 0)
     {
 
         $this->menuId = $menuId;
         return parent::save($id);
     }
 
-    public function del($menuId, $id = 0)
+    public function del($menuId = 0, $id = 0)
     {
         $this->menuId = $menuId;
         return parent::del($id);
     }
 
-    public function data($menuId)
+    public function data($menuId = 0)
     {
         $this->menuId = $menuId;
         return parent::data();
